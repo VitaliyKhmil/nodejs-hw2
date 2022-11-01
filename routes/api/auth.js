e868b4d20bb77b08ff4314680e74d24e9d2bb02e
VitaliyKhmil <hmilvitaliy@gmail.com>
2022-11-01 23:55:44 +0200

    add avatar rout, controler avatar apdate, file .gitkeep

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { validation, ctrlWrapper, authenticate } = require("../../middlewares");
+const { validation, ctrlWrapper, authenticate, upload} = require("../../middlewares");
 const { schemas } = require("../../models/user");
 const ctrl = require("../../controllers/auth");
 
@@ -9,6 +9,7 @@ const router = express.Router();
 router.post(
   "/register",
   validation(schemas.registerSchema),
+
   ctrlWrapper(ctrl.register)
 );
 
@@ -17,6 +18,8 @@ router.post("/login", validation(schemas.loginSchema), ctrlWrapper(ctrl.login));
 router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent));
 
 router.get("/logout", authenticate, ctrlWrapper(ctrl.logout));
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrlWrapper(ctrl.updateAvatar))
 
 router.patch(
   "/subscription",
